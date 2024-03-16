04548fe1792fc6a100b08aa4f65629828434f616
ChristianDoradoGarcia <ChristianDoradoGarcia@gmail.com>
2024-03-16 20:01:11 +0100

    Ahora sí, subimos el memset, antes no estaba en staged

--- Libft/ft_strlen.c
+++ Libft/ft_strlen.c
@@ -5,8 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cdorado- <cdorado-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/16 19:11:54 by marvin            #+#    #+#             */
-/*   Updated: 2024/03/16 19:11:54 by marvin           ###   ########.fr       */
+/*   Created: 2024/03/16 19:20:21 by cdorado-          #+#    #+#             */
+/*   Updated: 2024/03/16 19:20:21 by cdorado-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+int ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while(str[i]) 
+        i++;
+    return (i);
+}
+/*
+int main()
+{
+    char c[] = "M";
+    printf("El string tiene %d caracteres", ft_strlen(c));
+    return (0);
+}*/
