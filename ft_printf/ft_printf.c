15c67f87e11789a3b3e2a0cbf17502a3e3a4f6f9
Cristian Dorado Garcia <cdorado-@c1r4s1.42madrid.com>
2024-05-29 17:47:42 +0200

    Printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,13 +6,50 @@
 /*   By: cdorado- <cdorado-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 11:25:35 by cdorado-          #+#    #+#             */
-/*   Updated: 2024/05/26 11:26:37 by cdorado-         ###   ########.fr       */
+/*   Updated: 2024/05/26 17:04:19 by cdorado-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_printf(char const *, ....)
+int	ft_printf(char const *format, ...)
 {
-    
+	va_list pa;
+	char	*ptr;
+	int		len;
+
+	va_start(pa, format);
+	ptr = (char *)format;
+	len = ft_strlen(format);
+	while(*ptr != '\0')
+	{
+		if (*ptr != '%')
+		{
+			ft_putchar_fd(*ptr, 1);
+			ptr++;
+			continue;
+		}
+		switch (*++ptr)
+		{
+			case 'd':
+			ft_putnbr_fd(va_arg(pa, int), 1);
+			ptr++;
+			break;
+			case 's':
+			ft_putstr_fd(va_arg(pa, char *), 1);
+			ptr++;
+			break;
+		}
+	}
+	va_end(pa);
+	return (len);
+}
+
+int main()
+{
+	int prueba = printf("Hola %s\n", "amigo");
+	printf("El valor de retorno de printf es %d\n", prueba);
+	int prueba1 = ft_printf("Hola %s\n", "amigo");
+	printf("El valor de ft_printf es %d\n", prueba1);
+	return (0);
 }
